2b21de246ec4648b7bfe3c2ce4945a790b9c245f
Yury Frolov <yura200253@gmail.com>
2020-11-23 16:57:46 +0500

    B works

--- A.cpp
+++ A.cpp
@@ -18,50 +18,50 @@ int dfs(int current)
     return vertex_count + incidents[current].size() % 2;
 }
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-
-    int n, c = 0;
-    cin >> n;
-    vector<vector<pair<int, int>>> cuts(n);
-    for (int i = 0; i < n; ++i)
-    {
-        int k;
-        cin >> k;
-        cuts[i].resize(k);
-        vector<int> numbers_vec(k);
-        for (int j = 0; j < k; ++j)
-        {
-            cin >> cuts[i][j].first >> cuts[i][j].second;
-            if (numbers.count(cuts[i][j]) == 0)
-            {
-                numbers[cuts[i][j]] = c;
-                c++;
-            }
-            numbers_vec[j] = numbers[cuts[i][j]];
-        }
-        for (int j = 0; j < k - 1; ++j)
-        {
-            incidents[numbers_vec[j]].insert(numbers_vec[j + 1]);
-            incidents[numbers_vec[j + 1]].insert(numbers_vec[j]);
-        }
-    }
-    n = c;
-    visited.resize(n);
-    int linked_count = 0;
-    int count = 0;
-    for (int i = 0; i < n; ++i)
-    {
-        if (!visited[i])
-        {
-            linked_count++;
-            int inner_count = dfs(i);
-            count += max(0, inner_count / 2 - 1);
-        }
-    }
-    cout << count + linked_count - 1;
-    return 0;
-}
+//int main()
+//{
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    cout.tie(nullptr);
+//
+//    int n, c = 0;
+//    cin >> n;
+//    vector<vector<pair<int, int>>> cuts(n);
+//    for (int i = 0; i < n; ++i)
+//    {
+//        int k;
+//        cin >> k;
+//        cuts[i].resize(k);
+//        vector<int> numbers_vec(k);
+//        for (int j = 0; j < k; ++j)
+//        {
+//            cin >> cuts[i][j].first >> cuts[i][j].second;
+//            if (numbers.count(cuts[i][j]) == 0)
+//            {
+//                numbers[cuts[i][j]] = c;
+//                c++;
+//            }
+//            numbers_vec[j] = numbers[cuts[i][j]];
+//        }
+//        for (int j = 0; j < k - 1; ++j)
+//        {
+//            incidents[numbers_vec[j]].insert(numbers_vec[j + 1]);
+//            incidents[numbers_vec[j + 1]].insert(numbers_vec[j]);
+//        }
+//    }
+//    n = c;
+//    visited.resize(n);
+//    int linked_count = 0;
+//    int count = 0;
+//    for (int i = 0; i < n; ++i)
+//    {
+//        if (!visited[i])
+//        {
+//            linked_count++;
+//            int inner_count = dfs(i);
+//            count += max(0, inner_count / 2 - 1);
+//        }
+//    }
+//    cout << count + linked_count - 1;
+//    return 0;
+//}
--- B.cpp
+++ B.cpp
@@ -6,22 +6,25 @@ typedef long long ll;
 
 int VISITED = 100000;
 
-vector<unordered_set<int>> sort(vector<vector<int>> &graph, vector<int> &power) {
+vector<unordered_set<int>> sort(vector<vector<int>> &graph, vector<int> &power)
+{
     vector<unordered_set<int>> sorted;
     for (int i = 0; i < graph.size(); i++)
-        sorted.push_back(unordered_set<int>());
+        sorted.emplace_back();
     for (int i = 0; i < graph.size(); i++)
         sorted[power[i]].insert(i);
     return sorted;
 }
 
-int get_next_min_power(int min_power, vector<unordered_set<int>> &sorted) {
-    while (sorted[min_power].size() == 0)
+int get_next_min_power(int min_power, vector<unordered_set<int>> &sorted)
+{
+    while (sorted[min_power].empty())
         min_power++;
     return min_power;
 }
 
-vector<int> get_sorted_order(vector<vector<int>> &graph, vector<int> &power) {
+vector<int> get_sorted_order(vector<vector<int>> &graph, vector<int> &power)
+{
     vector<unordered_set<int>> sorted = sort(graph, power);
     vector<int> order;
     int min_power = *min_element(power.begin(), power.end());
@@ -52,6 +55,39 @@ int main()
     cin.tie(nullptr);
     cout.tie(nullptr);
 
+    int n;
+    cin >> n;
+    vector<vector<int>> graph(n);
+    vector<int> power(n);
+    for (int i = 0; i < n; ++i)
+    {
+        int k;
+        cin >> k;
+        power[i] = k;
+        for (int j = 0; j < k; ++j)
+        {
+            int incident;
+            cin >> incident;
+            incident--;
+            graph[i].push_back(incident);
+        }
+    }
+    vector<int> order = get_sorted_order(graph, power);
+    vector<int> colors(n);
+    for (auto vertex: order)
+    {
+        set<int> incident_colors;
+        for (auto incident: graph[vertex])
+        {
+            incident_colors.insert(colors[incident]);
+        }
+        int color = 1;
+        while(incident_colors.count(color) == 1)
+            color++;
+        colors[vertex] = color;
+    }
 
+    for (auto color: colors)
+        cout << color << "\n";
     return 0;
 }
