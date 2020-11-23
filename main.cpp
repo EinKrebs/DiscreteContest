903601fe55c5c0a810445934923df5df0fb5326a
Yury Frolov <yura200253@gmail.com>
2020-11-23 13:54:00 +0500

    Говно

--- main.cpp
+++ main.cpp
@@ -4,23 +4,12 @@ using namespace std;
 
 typedef long long ll;
 
-struct point
-{
-    int x;
-    int y;
-
-    bool operator == (point other) const
-    {
-        return this->x == other.x && this->y == other.y;
-    }
-};
-
 int main()
 {
     int n;
     cin >> n;
-    vector<vector<point>> cuts(n);
-    int dif = 0;
+    vector<vector<pair<int, int>>> cuts(n);
+    set<pair<int, int>> even_vertices;
     for (int i = 0; i < n; ++i)
     {
         int k;
@@ -28,11 +17,20 @@ int main()
         cuts[i].resize(k);
         for (int j = 0; j < k; ++j)
         {
-            cin >> cuts[i][j].x >> cuts[i][j].y;
+            cin >> cuts[i][j].first >> cuts[i][j].second;
         }
         if (!(cuts[i][0] == cuts[i][k - 1]))
-            dif += 1;
+        {
+            if(even_vertices.count(cuts[i][0]) == 1)
+                even_vertices.erase(cuts[i][0]);
+            else
+                even_vertices.insert(cuts[i][0]);
+            if(even_vertices.count(cuts[i][k - 1]) == 1)
+                even_vertices.erase(cuts[i][k - 1]);
+            else
+                even_vertices.insert(cuts[i][k - 1]);
+        }
     }
-    cout << max(0, dif - 1);
+    cout << max(0LL, ll(even_vertices.size()) / 2 - 1);
     return 0;
 }
