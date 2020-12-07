422dc51f8aa99e4977539793ca3831d5c0a31cfb
Yury Frolov <yura200253@gmail.com>
2020-12-07 19:21:02 +0500

    New contest

--- A.cpp
+++ A.cpp
@@ -4,64 +4,69 @@ using namespace std;
 
 typedef long long ll;
 
-vector<set<int>> incidents(100000);
-map<pair<int, int>, int> numbers;
-vector<bool> visited;
-
-int dfs(int current)
+bool bfs(string& word, vector<vector<vector<int>>>& incidents,
+         vector<int>& start, unordered_set<int>& terminals)
 {
-    int vertex_count = 0;
-    visited[current] = true;
-    for (auto point: incidents[current])
-        if (!visited[point])
-            vertex_count += dfs(point);
-    return vertex_count + incidents[current].size() % 2;
+    vector<unordered_set<int>> r(1);
+    for (auto starting: start)
+        r[0].insert(starting);
+    for (int i = 0; i < word.size(); i++)
+    {
+        r.emplace_back();
+        for (auto state: r[i])
+            for(auto incident: incidents[state][word[i] - 'a'])
+                r[i + 1].insert(incident);
+    }
+    for (auto finish: r[r.size() - 1])
+        if (terminals.count(finish))
+            return true;
+    return false;
 }
 
-//int main()
-//{
-//    ios::sync_with_stdio(false);
-//    cin.tie(nullptr);
-//    cout.tie(nullptr);
-//
-//    int n, c = 0;
-//    cin >> n;
-//    vector<vector<pair<int, int>>> cuts(n);
-//    for (int i = 0; i < n; ++i)
-//    {
-//        int k;
-//        cin >> k;
-//        cuts[i].resize(k);
-//        vector<int> numbers_vec(k);
-//        for (int j = 0; j < k; ++j)
-//        {
-//            cin >> cuts[i][j].first >> cuts[i][j].second;
-//            if (numbers.count(cuts[i][j]) == 0)
-//            {
-//                numbers[cuts[i][j]] = c;
-//                c++;
-//            }
-//            numbers_vec[j] = numbers[cuts[i][j]];
-//        }
-//        for (int j = 0; j < k - 1; ++j)
-//        {
-//            incidents[numbers_vec[j]].insert(numbers_vec[j + 1]);
-//            incidents[numbers_vec[j + 1]].insert(numbers_vec[j]);
-//        }
-//    }
-//    n = c;
-//    visited.resize(n);
-//    int linked_count = 0;
-//    int count = 0;
-//    for (int i = 0; i < n; ++i)
-//    {
-//        if (!visited[i])
-//        {
-//            linked_count++;
-//            int inner_count = dfs(i);
-//            count += max(0, inner_count / 2 - 1);
-//        }
-//    }
-//    cout << count + linked_count - 1;
-//    return 0;
-//}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n, sigma, s, t;
+    cin >> n >> sigma;
+    cin >> s;
+    vector<int> start(s);
+    for (int i = 0; i < s; ++i)
+        cin >> start[i];
+    cin >> t;
+    unordered_set<int> terminal;
+    for (int i = 0; i < t; i++)
+    {
+        int a;
+        cin >> a;
+        terminal.insert(a);
+    }
+    vector<vector<vector<int>>> incidents(n);
+    for (int i = 0; i < n; i++)
+    {
+        incidents[i].resize(26);
+        for (int j = 0; j < sigma; j++)
+        {
+            char letter;
+            int vertices;
+            cin >> letter >> vertices;
+            incidents[i][letter - 'a'].resize(vertices);
+            for (int k = 0; k < vertices; k++)
+                cin >> incidents[i][letter - 'a'][k];
+        }
+    }
+    int m;
+    cin >> m;
+    for (int i = 0; i < m; i++)
+    {
+        string word;
+        cin >> word;
+        if (bfs(word, incidents, start, terminal))
+            cout << "True\n";
+        else
+            cout << "False\n";
+    }
+    return 0;
+}
